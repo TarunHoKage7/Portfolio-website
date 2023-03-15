817c7f829476ae9f150d90a7f9c45cf271b0ebce
UserTarun <saikalyantarun2001@gmail.com>
2023-03-15 20:01:06 +0530

    induvidual projects page

--- js/main.js
+++ js/main.js
@@ -29,20 +29,132 @@ let induvidualProjectData = {
     web: {
         "worksy": {
             img: ".",
-            link: ".",
+            link: "https://github.com/TarunHoKage7/worksy",
             addnlPics: {
                 0: ".",
                 1: "."
             },
-            summary: ".",
+            summary: "Worksy is an upskilling, advertising, selling, and outreach application. It is also a place where you can find people from all sects of work for any tasks you have (like carpentry, plumbing, art, house help, caretakers, etc.) This application uses HTML, CSS, JS, MongoDB, ExpressJS, and NodeJS. The initial idea of this application was to help people from sectors where their primary source of advertising was through word of mouth. Using Worksy, they can advertise and display their work and experience and reach the eyes of potential clients.",
             techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+                3: "NodeJS",
+                4: "ExpressJS",
+                5: "MongoDB",
+            },
+        },
+
+        "Bus-Track": {
+            img: "/assets/bustrack.JPEG",
+            link: "https://github.com/TarunHoKage7/BusTrack",
+            addnlPics: {
+                0: "/assets/bustrack.JPEG",
+                1: "."
+            },
+            summary: "Bus-Track is a transportation tracking website. It lets us track our ride(cab, bus, bike, auto, etc.) in real-time. You can use Bus-Track to share your location with others, such as family and friends, so they can know where you are. It uses the built-in geolocation API to track your location(after you've allowed it to, of course). I've built this website coz I kept missing my college bus.!This website is not deployed yet.",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+                3: "NodeJS",
+                4: "ExpressJS",
+                5: "MongoDB",
+            },
+        },
+
+        "Tile-Game": {
+            img: ".",
+            link: "https://tilinator.netlify.app/",
+            addnlPics: {
                 0: ".",
                 1: "."
+            },
+            summary: "The tile game ('tile-inator', as I call it) is similar to the piano tile game. I built it coz I thought it was a great yet simple game. I have also added some Mario and Doofenshmirtz flavours to it. The tile-inator was built as a mobile-first game but can also be played on a desktop browser using keyboard and pointer clicks.",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+            },
+        },
+
+        "API-EXPERIMENTS": {
+            img: ".",
+            link: "https://myapiexpts.netlify.app/",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "This is my compilation of APIs that I have found to be interesting. I have built induvidual applications around these APIs. Though there isn't a lot of styling involved. ",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+            },
+        },
+
+        "FRONTEND-CHALLENGES": {
+            img: ".",
+            link: "https://frontendchallengesvskt.netlify.app",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "This website consists of a few of the frontend challenges that I have done. There are afew more at my codepen which will be updated into this site shortly.",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+            },
+        },
+
+        "WEB-CALCULATOR": {
+            img: ".",
+            link: "https://basicwebcalculator.netlify.app",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "This is a basic Arithmatic Calculator built for the browsers. This was one of the few that aren't built because of some problems I've faced or seen. Used HTML, CSS, and JS (image-map to build this calculator).",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
+            },
+        },
+
+        "NOTEPAD-LOCALSTORAGE": {
+            img: ".",
+            link: "https://localnotepad.netlify.app",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "A browser based notepad Application. This application is a local storage based notepad application. A user can create and use multiple notes and has access to basic font based styling options as of now. Currently built to use local storage for persistent memory.",
+            techStack: {
+                0: "HTML",
+                1: "CSS",
+                2: "JS",
             },
         },
     },
 
     android: {
+        "EXAMM-SNAPP": {
+            img: ".",
+            link: ".",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "Exam snap is an android application that was built to be a one-stop destination for all academic resources for everybody from my college. It was connected to MongoDB to allow retrieval and downloads for offline viewing capabilities. This app was built using flutter. It uses firebase to store and retrieve data.",
+            techStack: {
+                0: "DART",
+                1: "FLUTTER",
+                2: "FIREBASE",
+            },
+        }, 
+        
         "hospital-app": {
             img: ".",
             link: ".",
@@ -50,23 +162,67 @@ let induvidualProjectData = {
                 0: ".",
                 1: "."
             },
-            summary: ".",
+            summary: "I built this app during the COVID-19 pandemic. Most of the hospitals and people were suffering due to scarcity of resources and even more hardships for patients due to their need for complete isolation. It was to be a one-stop destination for anybody needing critical resources like food, medicine, oxygen, etc. I have built this app using Flutter.",
             techStack: {
-                0: ".",
-                1: "."
+                0: "DART",
+                1: "FLUTTER",
+                2: "FIREBASE",
             },
         },    
-    }
-    ,
-    iot: {
-        "rfid": {
+
+        "Calculator": {
             img: ".",
             link: ".",
             addnlPics: {
                 0: ".",
                 1: "."
             },
-            summary: ".",
+            summary: "This calculator was built to be connected to the cloud to retrieve all my previous calculations and queries.  It is built using flutter. It uses firebase to store all calculations in real time.",
+            techStack: {
+                0: "DART",
+                1: "FLUTTER",
+                2: "FIREBASE",
+            },
+        },    
+    }
+    ,
+    iot: {
+        "RFID-door-locking-system": {
+            img: ".",
+            link: ".",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "RFID door locking systems built with Arduino are becoming increasingly popular for securing homes, offices, and other establishments. This system works by allowing access only to those who have a valid RFID tag or card. When the user swipes their tag, the Arduino board reads the tag's unique identifier and compares it to a pre-programmed list of authorized users. If the tag is valid, the Arduino sends a signal to the locking mechanism, allowing the door to unlock. The system requires an RFID reader, a locking mechanism, an Arduino board, and some programming knowledge. The RFID reader reads the data from the RFID tag, and the Arduino board processes that data to determine whether access should be granted or not. If access is granted, the Arduino sends a signal to the locking mechanism to unlock the door.",
+            techStack: {
+                0: ".",
+                1: "."
+            },
+        },
+
+        "HOME-AUTOMATION-SYSTEM": {
+            img: ".",
+            link: ".",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "Home Automation:-A simple home automation system built over NodeMCU allows you to remotely control your home appliances using a Wi-Fi connection and a mobile application. The system uses a NodeMCU board, which is a low-cost microcontroller board that provides built-in Wi-Fi connectivity. To build the system, you will need some programming knowledge and experience working with electronics, as well as the Arduino IDE software tool. The system can help make your home more convenient and efficient while also providing a fun and educational project. To build a simple home automation system using NodeMCU, you will need to have some basic programming knowledge and experience working with electronics. You will also need to download and install the Arduino Integrated Development Environment (IDE), which is a software tool that is used to write and upload code to the NodeMCU board.",
+            techStack: {
+                0: ".",
+                1: "."
+            },
+        },
+
+        "Intruder-Detection-System": {
+            img: ".",
+            link: ".",
+            addnlPics: {
+                0: ".",
+                1: "."
+            },
+            summary: "An intruder detection system built over NodeMCU is a project that uses a combination of sensors, such as PIR motion sensors, to detect when someone enters a room or opens a door. The system is built using a NodeMCU board, which is a low-cost microcontroller board that provides built-in Wi-Fi connectivity. The system works by sending notifications to your phone or email when an intrusion is detected. To build the system, you will need some programming knowledge and experience working with electronics, as well as the Arduino IDE software tool. The system provides an added layer of security for your home or office and can be a fun and educational project to build. The intruder detection system built over NodeMCU can also be expanded to include additional sensors and features, such as a loud alarm or security camera integration. The system can be programmed to detect specific intrusion patterns or send notifications to different people depending on the time of day or the severity of the intrusion. With its built-in Wi-Fi connectivity, the system can also be accessed remotely, allowing you to check the status of your home or office from anywhere with an internet connection.",
             techStack: {
                 0: ".",
                 1: "."
